ba03649323b5d44049d9fb318e993717ecedfb38
Yagya Joshi <sevak365@gmail.com>
2021-07-04 00:48:11 -0400

    first gatsby commit

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Home() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>This is my first Gatsby Website</h1>
+      <p style={{ fontSize: "1.2rem", color: "purple" }}>
+        Gatsby is blazingly fast yet based on javascript functionality of
+        ultimate UI. This is because gatsby's runtime is based on react
+        framework. React is best in componentization of applications and this in
+        combination with blazingly fast gatsby makes this tech stack an enviable
+        combo for every develoeprs.
+      </p>
+    </div>
+  )
+}
